6f9f00108cea0b2d60753c4a5705879d5f2bcf36
Divya Shrestha <divyashrestha042@gmail.com>
2020-10-14 19:03:12 +0545

    combining the docker and adding setup.sh file

---
+++ docker/setup.sh
@@ -0,0 +1,13 @@
+set -e
+  
+echo "=== Copy .env  ==="
+cp .env.example .env
+echo "=== copy is done!  ==="
+
+echo "=== Execute docker-compose build... ==="
+docker-compose build
+echo "=== docker-compose build is done!! ==="
+
+echo "==== Generating the app key ==="
+docker-compose run app php artisan key:generate
+echo "==== App key generating is done!! ==="
