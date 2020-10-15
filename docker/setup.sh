233345543946a4eeb77a3e65cdb8d192ddc30d6e
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2020-10-15 09:21:36 +0545

    Update setup.sh
    composer install added

--- docker/setup.sh
+++ docker/setup.sh
@@ -8,6 +8,10 @@ echo "=== Execute docker-compose build... ==="
 docker-compose build
 echo "=== docker-compose build is done!! ==="
 
+echo "=== Installing composer... ==="
+composer install
+echo "=== Composer build is done!! ==="
+
 echo "==== Generating the app key ==="
 docker-compose run app php artisan key:generate
 echo "==== App key generating is done!! ==="
