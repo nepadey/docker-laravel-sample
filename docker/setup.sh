00a910034f2d195cfd67d7caea9f64d42ecb7db5
Divya Shrestha <53654225+anilnabin25@users.noreply.github.com>
2021-07-13 19:00:23 +0545

    updating the setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,7 +9,7 @@ docker-compose build
 echo "=== docker-compose build is done!! ==="
 
 echo "=== Installing composer... ==="
-composer install
+docker-compose run app composer update
 echo "=== Composer build is done!! ==="
 
 echo "==== Generating the app key ==="
